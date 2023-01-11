7e5c85b38a20556d7f45958008b251b519ba94d9
Nhân Trần <87871710+nartnahn1604@users.noreply.github.com>
2023-01-11 09:08:05 +0700

    finally

--- auto_generate_message.js
+++ auto_generate_message.js
@@ -3,6 +3,8 @@ const reader = new FileReader();
 var class_need_to_remind = [];
 var mailSection = document.getElementById("mail-section");
 mailSection.hidden = true;
+var table;
+var count_class = 1;
 function createMessage(num){
     remindStr = class_need_to_remind[num].split(",")
     teacherName = remindStr[remindStr.length - 1]
@@ -16,7 +18,72 @@ function createMessage(num){
     remind = remindStr[0].replace("-","") + "<br/>Hi " + teacherName +", I'm " +ptName+" - a " + position +" at Algorithmics. This message is for reminding you about filling in the CPR:<br/>" + content
     var mess_section = document.getElementById("message-section")
     mess_section.innerHTML +="<br/><br/>" + remind + "<br/>Thank you,<br/>" + ptName
-    mess_section.hidden = false
+}
+
+function generateForm(i){
+    return new Promise((res) => {
+        setTimeout(()=>{
+            var rows = table[0].querySelectorAll("tr")
+            r = rows[i];
+            // console.log(r);
+            url = r.childNodes[4].innerText.match(/(https:[^\s]+)/)
+            const _class = r.childNodes[4].innerText.match(/(NVH[^\s]+)/g)
+            if(_class){
+                window.open(url, '_blank')
+                var this_class = document.getElementsByName("class");
+                var this_lesson = document.getElementsByName("lesson");
+                var this_teacher = document.getElementsByName("teacher");
+                console.log(this_class)
+                this_class[0].value = _class;
+                this_lesson[0].value = r.childNodes[1].innerText;
+                this_teacher[0].value = r.childNodes[10].innerText;
+                // let status;
+                // window.focus();
+                // status = window.prompt(_class + " - next lesson: " + r.childNodes[1].innerText + "- teacher: " + r.childNodes[10].innerText)
+                // setTimeout(function(){
+                // }, 1000)
+                // window.focus();
+            }
+            res();
+        }, 300)
+    });
+}
+
+async function next_class(){
+    var status = document.getElementsByName("remind");
+    console.log(status[0])
+    var rows = table[0].querySelectorAll("tr")
+    r = rows[count_class];
+    const _class = r.childNodes[4].innerText.match(/(NVH[^\s]+)/g)
+    if(status[0].value.length > 0){
+        class_need_to_remind.push(_class + "," + status[0].value + "," + r.childNodes[10].innerText)
+        addClassNeedToRemind(_class + "," + status[0].value + "," + r.childNodes[10].innerText)
+        status[0].value = "";
+    }
+    count_class++;
+    await generateForm(count_class);
+    // console.log(count_class);
+    
+}
+function addClassNeedToRemind(_class){
+    var body = document.getElementsByTagName('tbody');
+    const remindStr = _class.split(",")
+    const teacherName = remindStr[remindStr.length - 1]
+    const className = remindStr[0].substring(0, remindStr[0].length - 4)
+    const lesson = remindStr[1]
+    var str = '<tr>'+
+            '<td>'+ (body[0].children.length + 1) +'</td>'+
+            '<td>' + className + '</td>'+
+            '<td>' + teacherName + '</td>'+
+            '<td>'+ lesson + '</td>'+
+            '<td>'+
+            '    <input id="messageBtn" onclick="createMessage(' +body[0].children.length+ ')" type="button" value="Create message">'+
+            '</td>'+
+            '<td>'+
+            '   <input type="checkbox" value="Add to mail remind">'+
+            '</td>'
+        '</tr>'
+    body[0].innerHTML += str;
 }
 document.addEventListener("DOMContentLoaded", () => {
     fileSelector.addEventListener('change', (event) => {
@@ -28,57 +95,9 @@ document.addEventListener("DOMContentLoaded", () => {
             
             // Parse the text
             var doc = parser.parseFromString(data, "text/html");
-            var table = doc.getElementsByTagName('table')
-            
-            var rows = table[0].querySelectorAll("tr")
-            var stopLoop = false
-            rows.forEach(r => {
-                if(stopLoop)
-                    return;
-                url = r.childNodes[4].innerText.match(/(https:[^\s]+)/)
-                const _class = r.childNodes[4].innerText.match(/(NVH[^\s]+)/g)
-                if(_class){
-                    window.open(url, '_blank')
-                    
-                    let status;
-                    window.focus();
-                    status = window.prompt(_class + " - next lesson: " + r.childNodes[1].innerText + "- teacher: " + r.childNodes[10].innerText)
-                    // setTimeout(function(){
-                    // }, 1000)
-                    // window.focus();
-                    if(status != null || status.length > 0){
-                        if(status != 0){
-                            class_need_to_remind.push(_class + "," + status + "," + r.childNodes[10].innerText)
-                            addClassNeedToRemind(_class + "," + status + "," + r.childNodes[10].innerText)
-                        }
-                    }
-                    else
-                        stopLoop = true;
-                }
-            })
+            table = doc.getElementsByTagName('table')
         });
     });
-    
-    function addClassNeedToRemind(_class){
-        var body = document.getElementsByTagName('tbody');
-        const remindStr = _class.split(",")
-        const teacherName = remindStr[remindStr.length - 1]
-        const className = remindStr[0].substring(0, remindStr[0].length - 4)
-        const lesson = remindStr[1]
-        var str = '<tr>'+
-                '<td>'+ (body[0].children.length + 1) +'</td>'+
-                '<td>' + className + '</td>'+
-                '<td>' + teacherName + '</td>'+
-                '<td>'+ lesson + '</td>'+
-                '<td>'+
-                '    <input id="messageBtn" onclick="createMessage(' +body[0].children.length+ ')" type="button" value="Create message">'+
-                '</td>'+
-                '<td>'+
-                '    <input type="checkbox" value="false">'+
-                '</td>'
-            '</tr>'
-        body[0].innerHTML += str;
-    }
     var mailBtn = document.getElementById("create-mail");
     mailBtn.addEventListener('click', function(){
         var mailbox = document.querySelectorAll('input[type="checkbox"]')
